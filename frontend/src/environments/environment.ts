e622f09da06592b1f4153b1f2b72380a28a80aff
Somunachimso Nzenwa <somunanzenwa@gmail.com>
2026-06-27 15:10:14 +0000

    done

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://miniature-space-palm-tree-x6v574xw54rhvr4x-8080.app.github.dev/api',
-  wsUrl: 'wss://miniature-space-palm-tree-x6v574xw54rhvr4x-8080.app.github.dev/ws',
+  apiUrl: 'https://crispy-train-699xr7p9449cxvx6.app.github.dev/api',
+  wsUrl: 'wss://crispy-train-699xr7p9449cxvx6.app.github.dev/ws',
   clerkPublishableKey: 'pk_test_d2FudGVkLWdlbGRpbmctOTEuY2xlcmsuYWNjb3VudHMuZGV2JA',
 };
