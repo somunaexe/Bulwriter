b64651325b8b437b05c450d5c34fd45cc5399645
Somunachimso Nzenwa <somunanzenwa@gmail.com>
2026-07-02 18:43:03 +0000

    using https now

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://bulwriter-production.up.railway.app/api',
-  wsUrl: 'ws://bulwriter-production.up.railway.app/ws',
+  apiUrl: 'https://bulwriter-production.up.railway.app/api',
+  wsUrl: 'wss://bulwriter-production.up.railway.app/ws',
   clerkPublishableKey: 'pk_test_d2FudGVkLWdlbGRpbmctOTEuY2xlcmsuYWNjb3VudHMuZGV2JA',
 };
