cbd2a90174d03482298b871f40dbebe3e0c05954
Somunachimso Nzenwa <somunanzenwa@gmail.com>
2026-07-22 19:10:34 +0000

    commit

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://crispy-train-699xr7p9449cxvx6.app.github.dev/api',
-  wsUrl: 'wss://crispy-train-699xr7p9449cxvx6.app.github.dev/ws',
+  apiUrl: 'https://*.app.github.dev/api',
+  wsUrl: 'wss://*.app.github.dev/ws',
   clerkPublishableKey: 'pk_test_d2FudGVkLWdlbGRpbmctOTEuY2xlcmsuYWNjb3VudHMuZGV2JA',
 };
