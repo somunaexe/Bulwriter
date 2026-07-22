e89f463356a53bb8a889939338b6347561c115ad
Somunachimso Nzenwa <somunanzenwa@gmail.com>
2026-07-22 19:59:13 +0000

    expose backend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://*.app.github.dev/api',
-  wsUrl: 'wss://*.app.github.dev/ws',
+  apiUrl: 'https://redesigned-fishstick-6649qpwg47q25pv9-8080.app.github.dev/api',
+  wsUrl: 'wss://redesigned-fishstick-6649qpwg47q25pv9-8080.app.github.dev/ws',
   clerkPublishableKey: 'pk_test_d2FudGVkLWdlbGRpbmctOTEuY2xlcmsuYWNjb3VudHMuZGV2JA',
 };
